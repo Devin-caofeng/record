92c26c155ad0c625df9b901ba6606143921c84f9
devincaofeng <devin.caofeng@gmail.com>
2018-03-28 17:31:42 +0800

    49_group_anagrams

--- OJ/LeetCode/49_group_anagrams.cpp
+++ OJ/LeetCode/49_group_anagrams.cpp
@@ -2,12 +2,40 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_map>
+#include <set>
 #include <algorithm>
 using std::string; using std::vector;
 using std::cout; using std::endl;
+using std::unordered_map;
+using namespace std;
 
 class Solution {
 public:
+    vector<vector<string>> groupAnagrams2(vector<string>& strs) {
+        unordered_map<string, multiset<string> mp;
+        for (string s : strs) {
+            string t = strSort(s);
+            mp[t].insert(s);
+        }
+    }
+
+    string strSort(string s) {
+        int cnt[26] = { 0 }, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            ++cnt[s[i] - 'a'];
+        }
+
+        int p = 0;
+        string t(n, 'a');
+        for (int j = 0; j < 26; ++j) {
+            for (int i = 0; i < cnt[j]; ++i) {
+                t[p++] += j;
+            }
+        }
+        return t;
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         std::map<int, int> str_index;
         int cnt = 0;
@@ -20,6 +48,28 @@ public:
             ret[str_index[getStrSum(iter)]].push_back(iter);
         }
 
+        return ret;
+    }
+
+    vector<string> anagrams(vector<string> &strs) {
+        unordered_map<string, vector<string>::iterator> cache;
+        vector<string> ret;
+        for (auto iter = strs.begin(); iter != strs.end(); ++iter) {
+            string temp(*iter);
+            std::sort(temp.begin(), temp.end());
+
+            auto found = cache.find(temp);
+            if (found == cache.end()) {
+                cache[temp] = iter;
+            }
+            else {
+                ret.push_back(*iter);
+                if (found->second != strs.end()) {
+                    ret.push_back(*found->second);
+                    found->second = strs.end();
+                }
+            }
+        }
         return ret;
     }
 
