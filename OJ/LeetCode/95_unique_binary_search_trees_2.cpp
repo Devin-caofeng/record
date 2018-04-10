e8ba50620407537123d92708da1f262ebe3c99dc
devincaofeng <devin.caofeng@gmail.com>
2018-04-10 20:21:51 +0800

    98

--- OJ/LeetCode/95_unique_binary_search_trees_2.cpp
+++ OJ/LeetCode/95_unique_binary_search_trees_2.cpp
@@ -5,6 +5,28 @@
 #include "tree_node.hpp"
 using namespace std;
 
+vector<TreeNode *> helper(int start, int end) {
+    vector<TreeNode *> result;
+    if (start > end) {
+        result.push_back(NULL);
+        return result;
+    }
+
+    for (int i = start; i <= end; ++i) {
+        vector<TreeNode *> left_tree = helper(start, i - 1);
+        vector<TreeNode *> right_tree = helper(i + 1, end);
+        for (size_t j = 0; j < left_tree.size(); ++j) {
+            for (size_t k = 0; k < right_tree.size(); ++k) {
+                TreeNode *root = new TreeNode(i);
+                root->left = left_tree[j];
+                root->right = right_tree[k];
+                result.push_back(root);
+            }
+        }
+    }
+    return result;
+}
+
 vector<TreeNode *> generateTree(int from, int to) {
     vector<TreeNode *> ret;
     if (to - from < 0) ret.push_back(0);
---
+++ OJ/LeetCode/98_validate_binary_search_tree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "tree_node.hpp"
+using namespace std;
+
+bool isValidBST(TreeNode *root) {
+    if (root == NULL) return true;
+    return helper(root, )
+}
+
+int main() {
+    return 0;
+}
