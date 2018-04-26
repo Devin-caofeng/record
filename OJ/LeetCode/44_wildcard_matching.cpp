97445683108f671ac0d1fad7e4eec1272638d7c0
devincaofeng <devin.caofeng@gmail.com>
2018-04-26 20:36:04 +0800

    correct 44_wildcard_matching

--- OJ/LeetCode/44_wildcard_matching.cpp
+++ OJ/LeetCode/44_wildcard_matching.cpp
@@ -5,7 +5,27 @@
 using namespace std;
 
 bool isMatch(string s, string p) {
+    int star = 0, ss = 0, i = 0, j = 0;
+    while (s[i]) {
+        if (p[j] == '?' || p[j] == s[i]) {
+            j++; i++;
+            continue;
+        }
+        if (p[j] == '*') {
+            star = ++j;
+            ss = i;
+            continue;
+        }
+        if (star) {
+            j = star;
+            i = ++ss;
+            continue;
+        }
+        return false;
+    }
+    while (p[j] == '*') j++;
 
+    return !p[j];
 }
 
 int main() {
