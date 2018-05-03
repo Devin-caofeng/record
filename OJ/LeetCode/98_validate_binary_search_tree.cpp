4b9ea5ba9abf9aab2c9c8527a42928d7206bf782
devincaofeng <devin.caofeng@gmail.com>
2018-05-03 22:06:17 +0800

    repair 98, 99

--- OJ/LeetCode/98_validate_binary_search_tree.cpp
+++ OJ/LeetCode/98_validate_binary_search_tree.cpp
@@ -5,10 +5,25 @@
 #include "tree_node.hpp"
 using namespace std;
 
-bool isValidBST(TreeNode *root) {
-    if (root == NULL) return true;
-    return helper(root, )
-}
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, false, false, 0, 0);
+    }
+
+    bool isValidBST(TreeNode *root, bool left, bool right, int min, int max) {
+        if (!root) {
+            return true;
+        }
+        else if ((left && root->val <= min) || (right && root->val >= max)) {
+            return false;
+        }
+        else {
+            return isValidBST(root->left, left, true, min, root->val) &&
+                   isValidBST(root->right, true, right, root->val, max);
+        }
+    }
+};
 
 int main() {
     return 0;
--- OJ/LeetCode/99_recover_binary_search_tree.cpp
+++ OJ/LeetCode/99_recover_binary_search_tree.cpp
@@ -2,7 +2,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "tree_node.hpp"
 using namespace std;
+
+class Solution {
+public:
+    void recoverTree(TreeNode *root) {
+        vector<TreeNode *> lst;
+        vector<int>        vals;
+
+        inorder(root, lst, vals);
+        sort(vals.begin(), vals.end());
+        for (size_t i = 0; i < lst.size(); ++i) {
+            lst[i]->val = vals[i];
+        }
+    }
+
+private:
+    void inorder(const TreeNode *root, vector<TreeNode *> &lst,
+                 vector<int> &vals) {
+        if (!root) return;
+        inorder(root->left, lst, vals);
+        lst.push_back(const_cast<TreeNode *>(root));
+        vals.push_back(root->val);
+        inorder(root->right, lst, vals);
+    }
+
+};
 
 int main() {
     return 0;
