c2c7ef64567cca05c4ea9d59ac77092f8313b077
devincaofeng <devin.caofeng@gmail.com>
2018-05-05 22:49:06 +0800

    repair 99_recover_binary_search_tree

--- OJ/LeetCode/99_recover_binary_search_tree.cpp
+++ OJ/LeetCode/99_recover_binary_search_tree.cpp
@@ -30,6 +30,44 @@ private:
 
 };
 
+class S2 {
+public:
+    void recoverTree(TreeNode *root) {
+        TreeNode *firs = nullptr, *secd = nullptr, *parent = nullptr;
+        TreeNode *cur, *pre;
+        cur = root;
+
+        while (cur) {
+            if (!cur->left) {
+                if (parent && parent->val > cur->val) {
+                    if (!firs) firs = parent;
+                    secd = cur;
+                }
+                parent = cur;
+                cur = cur->right;
+            }
+            else {
+                pre = cur->left;
+                while (pre->right && pre->right != cur) pre = pre->right;
+                if (!pre->right) {
+                    pre->right = cur;
+                    cur = cur->left;
+                }
+                else {
+                    pre->right = nullptr;
+                    if (parent->val > cur->val) {
+                        if (!firs) firs = parent;
+                        secd = cur;
+                    }
+                    parent = cur;
+                    cur = cur->right;
+                }
+            }
+        }
+        if (firs && secd) swap(firs->val, secd->val);
+    }
+};
+
 int main() {
     return 0;
 }
