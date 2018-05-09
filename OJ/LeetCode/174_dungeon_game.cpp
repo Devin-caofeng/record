1263a1a890a7f4ec71a2002c5d2bba0b9ac18a1a
devincaofeng <devin.caofeng@gmail.com>
2018-05-09 18:35:43 +0800

    174_dungeon_game

---
+++ OJ/LeetCode/174_dungeon_game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int calculateMinmumHP(vector<vector<int>> &dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m][n - 1] = dp[m - 1][n] = 1;
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) -
+                                  dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
+int main() {
+    return 0;
+}
