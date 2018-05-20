e2cac01c1fba90325a8b1625ef0b5e2d65554105
devincaofeng <devin.caofeng@gmail.com>
2018-05-20 22:23:18 +0800

    102_binary_tree_level_order_traversal.cpp

---
+++ OJ/LeetCode/102_binary_tree_level_order_traversal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include "tree_node.hpp"
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> leveclOrder(TreeNode *root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+
+        queue<TreeNode *> que;
+        que.push(root);
+
+        while (!que.empty()) {
+            vector<int> tmp(que.size());
+
+            size_t n = que.size();
+            for (size_t i = 0; i < n; ++i) {
+                TreeNode *cur = que.front(); que.pop();
+                tmp[i] = cur->val;
+
+                if (cur->left) que.push(cur->left);
+                if (cur->right) que.push(cur->right);
+            }
+
+            res.push_back(std::move(tmp));
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
