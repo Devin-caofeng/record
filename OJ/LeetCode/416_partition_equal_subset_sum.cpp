9141072677068b58285be4d2e5724dbed2388d21
devincaofeng <devin.caofeng@gmail.com>
2018-06-30 21:56:27 +0800

    416_partition_equal_subset_sum.cpp

---
+++ OJ/LeetCode/416_partition_equal_subset_sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum & 1) return false;
+
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = target; i >= num; --i) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+};
+
+int main() {
+    return 0;
+}
